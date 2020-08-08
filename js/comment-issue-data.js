ef57520064942e748580091ae836b32ffc83a2d8
ComeBey <hasan.wang520@gmail.com>
2020-08-08 10:57:18 +0800

    Site updated: 2020-08-08 10:57:16

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -191,7 +191,7 @@ function loadIndexHotData() {
                 } else {
                     classDiv = "class=\"item level0\"";
                 }
-                hotContent += "<a href =\"" + item.body.substr(0, item.body.indexOf("\n") - 1) + "\"target=\"_blank\"" + classDiv + ">" + item.title.substr(0, item.title.indexOf("-") - 1) + "&nbsp;🔥" + (item.comments * 101) + "</a>&nbsp;&nbsp;"
+                hotContent += "<a href =\"" + item.body.substr(0, item.body.indexOf("\n") - 1) + "\"target=\"_blank\"" + classDiv + ">" + item.title.substr(0, item.title.indexOf("-") - 1) + "&nbsp;🔥" + (item.comments) + "</a>&nbsp;&nbsp;"
             })
             hotDiv.html("");
             if (hotContent == "") {
