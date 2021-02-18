ada75e26ca4e8de032c5fb77d78687b10811bcaa
ComeBey <hasan.wang520@gmail.com>
2021-02-18 15:23:21 +0800

    Site updated: 2021-02-18 15:23:18

--- en/js/main.js
+++ en/js/main.js
@@ -2,19 +2,13 @@
     $('.article img:not(".not-gallery-item")').each(function () {
         // wrap images with link and add caption if possible
         if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            //$(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            $(this).wrap('<a class="gallery-item" style="display:block;text-align:center;" href="' + $(this).attr('src') + '"></a>');
             if (this.alt) {
                 $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
             }
         }
     });
-
-    //if (typeof (moment) === 'function') {
-    //    $('.article-meta time').each(function () {
-    //        $(this).text(moment($(this).attr('datetime')).fromNow());
-    //    });
-    //}
-
     $('.article > .content > table').each(function () {
         if ($(this).width() > $(this).parent().width()) {
             $(this).wrap('<div class="table-overflow"></div>');
